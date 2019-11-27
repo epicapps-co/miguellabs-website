29276db25678867aeeac15558eae91e8c89ff499
herkulano <hi@herkulano.com>
2019-11-27 13:43:12 +0200

    feat: add grid

---
+++ src/components/styled.js
@@ -0,0 +1,22 @@
+import styled from "styled-components"
+import sizes from "../global/sizes"
+
+export const Box = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-template-rows: auto 1fr;
+  grid-column-gap: var(--column-gap);
+
+  margin: auto;
+  padding: 0 var(--column-gap);
+  width: 100%;
+  max-width: var(--max-width);
+  height: 100%;
+
+  z-index: 10;
+
+  @media (max-width: ${sizes.sm}) {
+    padding: 0 var(--column-gap-sm);
+    grid-template-columns: repeat(3, 1fr);
+  }
+`
